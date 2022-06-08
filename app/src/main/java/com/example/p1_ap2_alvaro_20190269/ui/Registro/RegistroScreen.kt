52e55e37d7f9142c3e9d135f9476e88a9a4b63a0
Alvaro S. Hernandez Luzon <alvarostarling24@gmail.com>
2022-06-08 01:13:20 -0400

    Agregando RegistroScreen.kt

---
+++ app/src/main/java/com/example/p1_ap2_alvaro_20190269/ui/Registro/RegistroScreen.kt
@@ -0,0 +1,48 @@
+package com.example.p1_ap2_alvaro_20190269.ui.Registro
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.absolutePadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun RegistroScreen() {
+    Scaffold(
+
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "Registro")
+                },
+                navigationIcon = {
+                    Icon(
+                        imageVector = Icons.Default.ArrowBack,
+                        contentDescription = "ArrowBack",
+                        modifier = Modifier.clickable {
+
+                        }
+                    )
+                }
+            )
+        }
+
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(it)
+                .absolutePadding(16.dp, 16.dp, 16.dp, 16.dp)
+        ) {
+
+        }
+
+    }
+}

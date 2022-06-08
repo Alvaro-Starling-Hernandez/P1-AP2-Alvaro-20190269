e20f616fbf0956a31a17270360fed5d868bd54d3
Alvaro S. Hernandez Luzon <alvarostarling24@gmail.com>
2022-06-08 19:38:07 -0400

    Modificando ConsultaScreen.kt

--- app/src/main/java/com/example/p1_ap2_alvaro_20190269/ui/Prestamo/ConsultaScreen.kt
+++ app/src/main/java/com/example/p1_ap2_alvaro_20190269/ui/Prestamo/ConsultaScreen.kt
@@ -1,25 +1,33 @@
 package com.example.p1_ap2_alvaro_20190269.ui.Consulta
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.absolutePadding
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import com.example.p1_ap2_alvaro_20190269.ui.Prestamo.PrestamoViewModel
+import com.example.p1_ap2_alvaro_20190269.ui.componentes.RowPrestamo
 import com.example.p1_ap2_alvaro_20190269.utils.Screens
 
 @Composable
-fun ConsultaScreen(navHostController: NavHostController) {
+fun ConsultaScreen(
+    viewModel: PrestamoViewModel = hiltViewModel(),
+    navHostController: NavHostController
+) {
     Scaffold(
 
         topBar = {
                  TopAppBar(
                      title = {
-                         Text("Consulta")
+                         Text("Lista de Prestamos")
                      }
                  )
         },
@@ -38,9 +46,23 @@ fun ConsultaScreen(navHostController: NavHostController) {
         Column(
             modifier = Modifier
                 .padding(it)
+                .absolutePadding(16.dp, 16.dp, 16.dp, 16.dp)
 
         ) {
+            val listaPrestamos = viewModel.prestamos.collectAsState(initial = emptyList())
 
+            LazyColumn(modifier = Modifier.fillMaxWidth()) {
+                items(listaPrestamos.value){prestamo ->
+                    RowPrestamo(prestamo = prestamo, navHostController = navHostController)
+                    Divider(
+                        color = Color.Black,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .width(1.dp)
+                    )
+                    Spacer(modifier = Modifier.height(15.dp))
+                }
+            }
         }
     }
 }

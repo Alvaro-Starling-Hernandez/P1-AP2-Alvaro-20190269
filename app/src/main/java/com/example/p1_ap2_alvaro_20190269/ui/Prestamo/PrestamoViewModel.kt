fa2957971cc2053e63beff402ccf6f81eaa5b687
Alvaro S. Hernandez Luzon <alvarostarling24@gmail.com>
2022-06-08 18:56:14 -0400

    Agregando anotacion @HiltViewModel

--- app/src/main/java/com/example/p1_ap2_alvaro_20190269/ui/Prestamo/PrestamoViewModel.kt
+++ app/src/main/java/com/example/p1_ap2_alvaro_20190269/ui/Prestamo/PrestamoViewModel.kt
@@ -8,20 +8,22 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.p1_ap2_alvaro_20190269.data.PrestamoRepository
 import com.example.p1_ap2_alvaro_20190269.model.Prestamo
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+@HiltViewModel
 class PrestamoViewModel @Inject constructor(
     val prestamoRepository: PrestamoRepository
-): ViewModel() {
+) : ViewModel() {
     var deudor by mutableStateOf("")
     var concepto by mutableStateOf("")
     var monto by mutableStateOf("")
 
     var prestamos = prestamoRepository.getList()
-    private set
+        private set
 
-    fun Guardar(){
+    fun Guardar() {
         viewModelScope.launch {
             prestamoRepository.insertar(
                 Prestamo(

25783d2e1b8da744cb634f3e17ec1b4c77e2af26
Alvaro S. Hernandez Luzon <alvarostarling24@gmail.com>
2022-06-08 18:46:23 -0400

    Agregando PrestamoViewModel.kt

---
+++ app/src/main/java/com/example/p1_ap2_alvaro_20190269/ui/Prestamo/PrestamoViewModel.kt
@@ -0,0 +1,35 @@
+package com.example.p1_ap2_alvaro_20190269.ui.Prestamo
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.p1_ap2_alvaro_20190269.data.PrestamoRepository
+import com.example.p1_ap2_alvaro_20190269.model.Prestamo
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+class PrestamoViewModel @Inject constructor(
+    val prestamoRepository: PrestamoRepository
+): ViewModel() {
+    var deudor by mutableStateOf("")
+    var concepto by mutableStateOf("")
+    var monto by mutableStateOf("")
+
+    var prestamos = prestamoRepository.getList()
+    private set
+
+    fun Guardar(){
+        viewModelScope.launch {
+            prestamoRepository.insertar(
+                Prestamo(
+                    deudor = deudor,
+                    concepto = concepto,
+                    monto = monto.toFloat()
+                )
+            )
+        }
+    }
+}
